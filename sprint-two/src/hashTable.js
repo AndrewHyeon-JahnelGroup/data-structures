e4b90a79dd3de0d63d7f9ac1e2a11f889d66bc23
Hack Reactor Student <student@hackreactor.com>
2017-07-07 10:46:14 -0700

    Complete hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,44 +1,51 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray( this._limit );
+  this._count = 0;
 };
 
 HashTable.prototype.insert = function( k, v ) {
   var index = getIndexBelowMaxForKey( k, this._limit );
-
-  if ( this._storage.get( index ) === undefined || this._storage[ index ] === k ) {
-    this._storage.set( index, v );
-  } else {
-    for ( index; index !== this._limit; index++ ) {
-      if ( this._storage.get( index ) === undefined ) {
-        this._storage.set( index, v );
+  var bucket = this._storage.get( index );
+    
+  if ( !bucket ) {
+    this._storage.set( index, [ [ k, v ] ] );
+  } else {    
+    for ( var i = 0; i < bucket.length; i++ ) {
+      if ( bucket[ i ][ 0 ] === k) {
+        bucket[ i ][ 1 ] = v;
+      } else { 
+        bucket.push( [ k, v ] );
       }
-    }
+    } 
+    
+    this._storage.set( index, bucket );
   }
 
-  this._storage[ index ] = k;
-  console.log( this._storage[ index ] );
+  this._count++;
 };
 
 HashTable.prototype.retrieve = function( k ) {
   var index = getIndexBelowMaxForKey( k, this._limit );
-  
-  if ( this._storage[ index ] === k ) { 
-    return this._storage.get( index );
-  }
-
- 
-  for ( index; index !== this._limit; index++ ) {
-    if ( this._storage[ index ] === k ) {
-      return this._storage.get( index );
+  var bucket = this._storage.get( index );
+  for ( var i = 0; i < bucket.length; i++ ) {
+    if ( bucket[ i ][ 0 ] === k ) {
+      return bucket[ i ][ 1 ];
     }
   }
+
 };
 
 HashTable.prototype.remove = function( k ) {
   var index = getIndexBelowMaxForKey( k, this._limit );
-
-  this._storage.set( index, undefined );
+  var bucket = this._storage.get( index );
+  
+  for(var i= 0; i < bucket.length; i++ ) {
+    if(bucket[ i ][ 0 ] === k ){
+      bucket.splice( i, 1 );
+      this._count--;
+    }
+  }
 };
 
 /*
