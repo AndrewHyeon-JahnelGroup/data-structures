aabec76d3080e76bc8c7762405c08cc4b9f1f0cb
Hack Reactor Student <student@hackreactor.com>
2017-07-07 16:54:34 -0700

    Complete binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,70 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function( value ) {
+  var node = Object.create( BinarySearchTree.prototype );
+
+  node.value = value;
+  node.left = undefined;
+  node.right = undefined;
+
+  return node;
 };
 
+BinarySearchTree.root = undefined;
+
+BinarySearchTree.prototype.insert = function( value, parent ) {
+  if ( !this.root ) {
+    this.root = this;
+  }
+
+  var currentNode = parent ? parent : this.root
+
+  if ( value < currentNode.value ) {
+    if ( currentNode.left ) {
+      this.insert( value, currentNode.left );
+    } else { 
+      currentNode.left = BinarySearchTree( value );
+    } 
+  }
+
+  if ( value > currentNode.value ) {
+    if ( currentNode.right ) {
+      this.insert( value, currentNode.right );
+    } else { 
+      currentNode.right = BinarySearchTree( value );
+    } 
+  }
+};
+
+BinarySearchTree.prototype.contains = function( value ) {
+  var current = this.root;
+
+  while ( current ) {
+    if ( value === current.value ) {
+      return true;
+    } else if ( value < current.value ) {
+      current = current.left;
+    } else {
+      current = current.right;
+    }
+  }
+
+  return false;   
+};
+
+BinarySearchTree.prototype.depthFirstLog = function( callBack, parent ) {
+  var currentNode = parent ? parent : this.root;
+  
+  if ( currentNode ) {
+    callBack( currentNode.value );
+  }
+
+  if ( currentNode.left ) {
+    this.depthFirstLog( callBack, currentNode.left );
+  }
+
+  if ( currentNode.right ) {
+    this.depthFirstLog( callBack, currentNode.right );
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
