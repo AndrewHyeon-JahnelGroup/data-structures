3205b4a3bb2fe878f9930a106ab073118f01b6ce
Hack Reactor Student <student@hackreactor.com>
2017-07-08 17:02:17 -0700

    Enable resizing tests and work on hashTable.rehash

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -15,7 +15,7 @@ HashTable.prototype.insert = function( k, v ) {
     for ( var i = 0; i < bucket.length; i++ ) {
       if ( bucket[ i ][ 0 ] === k) {
         bucket[ i ][ 1 ] = v;
-      } else { 
+      } else {
         bucket.push( [ k, v ] );
       }
     } 
@@ -24,6 +24,10 @@ HashTable.prototype.insert = function( k, v ) {
   }
 
   this._count++;
+
+  if ( this._count >= 3 * this._limit / 4 ) {
+    this.rehash();
+  }
 };
 
 //avg: O(1) worst: O(n)
@@ -47,7 +51,43 @@ HashTable.prototype.remove = function( k ) {
     if (bucket[ i ][ 0 ] === k ) {
       bucket.splice( i, 1 );
       this._count--;
+
+      if ( (this._count === this._limit / 4) && (this._limit > 8) ) {
+        this.rehash();
+      }
     }
+  }
+};
+
+HashTable.prototype.rehash = function() {
+  var tempStorage = [];
+
+  for ( var index = 0; index !== this._limit; index++ ) { 
+    var bucket = this._storage.get( index );
+ 
+    if ( bucket ) {
+      for ( var i = 0; i !== bucket.length; i++ ) {
+        tempStorage.push( bucket[ i ] );
+      }
+    }
+  }
+
+  console.log( tempStorage );
+
+  if ( this._count === 3 * this._count / 4) {
+    this._limit = 2 * this._limit;
+
+    this._storage = LimitedArray( this._limit );
+  }
+
+  if ( (this._count === this._count / 4) && (this._limit > 8) ) {
+    this._limit = this.limit / 2;
+
+    this._storage = LimitedArray( this._limit );
+  }
+
+  for ( var i = 0; i !== tempStorage.length; i++ ) {
+    this.insert( tempStorage[ i ][ 0 ], tempStorage[ i ][ 1 ] );
   }
 };
 
